91ff5587fa43151c189e516c8fb3611636e66f37
simplemonkey <garimellagss@gmail.com>
2025-05-07 18:31:37 +0200

    App version 2 deployment

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from Kubernetes!'));
+app.get('/', (req, res) => res.send('Hello from Kubernetes - version2!'));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
