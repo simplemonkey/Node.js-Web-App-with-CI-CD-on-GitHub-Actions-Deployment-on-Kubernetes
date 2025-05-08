85b1f12b1dfb29792ad1a7e986254681be9ec2ff
simplemonkey <garimellagss@gmail.com>
2025-05-08 14:49:29 +0200

    terraform deploy

---
+++ infra/main.tf
@@ -0,0 +1,34 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  dns_prefix = "nodejsaks"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
---
+++ infra/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "nodejs-rg"
+}
+
+variable "location" {
+  default = "Central US"
+}
+
+variable "aks_name" {
+  default = "nodejs-aks"
+}
+
+variable "acr_name" {
+  default = "nodejsacr123"
+}
