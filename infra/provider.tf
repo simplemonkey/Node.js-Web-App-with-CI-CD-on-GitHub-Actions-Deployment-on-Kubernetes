7cb318b73b99b372b8d9e30122d3a3265c8dfdbe
simplemonkey <garimellagss@gmail.com>
2025-05-08 15:30:01 +0200

    fix "terraform", "azurerm" providers

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = var.resource_group_name       # Replace with your resource group name
+    resource_group_name  = "nodejs-rg"       # Replace with your resource group name
     storage_account_name = "mytfstate"            # Replace with your storage account name
     container_name       = "tfstate"              # Replace with your container name
     key                  = "terraform.tfstate"    # The name of the state file
